994a00cc4ce73109249d840bb21803a94feafdb0
Brad Wilcox <brad_wilcox@me.com>
2013-11-17 22:35:10 -0500

    adding Foundation front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,7 @@
 class VisitorsController < ApplicationController
 
   def new
-    @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
+@owner = Owner.new
   end
 
 end
