b64b08353be4ed589bfea883677fa179b7fffa9f
Brad Wilcox <brad_wilcox@me.com>
2013-11-17 22:47:24 -0500

    adding 'about' and 'contact' pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 LearnRails::Application.routes.draw do
   root to: 'visitors#new'
+
+  # get "/pages/*id", to: 'pages#show'
+  # root to: 'visitors#new'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
